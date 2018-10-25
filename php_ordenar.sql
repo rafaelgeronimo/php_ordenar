3891bd90b7090c5b93f61556a4a2a2d5003d974f
Rafael Geronimo <rafaelgeronimo@outlook.com>
2018-10-25 13:05:49 -0300

    Atualizado arquivo do BD - incluindo dados

--- php_ordenar.sql
+++ php_ordenar.sql
@@ -11,7 +11,16 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
--- Exportação de dados foi desmarcado.
+-- Copiando dados para a tabela php_ordenar.usuarios: ~5 rows (aproximadamente)
+/*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
+INSERT IGNORE INTO `usuarios` (`id`, `nome`, `idade`) VALUES
+	(1, 'Eduardo', 19),
+	(2, 'Rafael', 35),
+	(3, 'Camila', 28),
+	(4, 'Thiago', 22),
+	(5, 'Enzo', 6);
+/*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
+
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
